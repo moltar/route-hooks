cd3f858830cb2343814b116679b6b7feee3e194c
Roman Filippov <rf@romanfilippov.com>
2020-05-05 19:35:11 +0700

    build: adds debugger config

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,7 +47,10 @@ export default {
     /*
     ** You can extend webpack config here
     */
-    extend (config, ctx) {
+    extend(config, ctx) {
+      if (ctx.isDev) {
+        config.devtool = ctx.isClient ? 'source-map' : 'inline-source-map'
+      }
     }
   }
 }
