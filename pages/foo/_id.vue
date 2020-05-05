3a84e94dbbc74784801810b666b19382abc29c53
Roman Filippov <rf@romanfilippov.com>
2020-05-05 18:53:41 +0700

    feat: demo

---
+++ pages/foo/_id.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    Page ID: {{ id }}
+
+    <hr />
+
+    <NuxtLink :to="{ name: 'foo-id', params: { id: id + 1 } }">Next Page</NuxtLink>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  data() {
+    return {
+      id: Number,
+    }
+  },
+  asyncData({ params }) {
+    console.log('asyncData')
+
+    return {
+      id: Number(params.id)
+    }
+  },
+  beforeRouteUpdate(to, from, next) {
+    console.log('beforeRouteUpdate')
+    next()
+  },
+})
+</script>
